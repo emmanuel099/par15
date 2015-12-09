3e16269d4b79a22334734e120fa3c0b8fd68ce21
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-12-09 22:46:12 +0100

    add openmp stencil build files

---
+++ stencil_openmp/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <omp.h>
+
+#include <stencil/matrix.h>
+
+int main(int argc, char **argv)
+{ 
+    stencil_matrix_t *matrix = stencil_matrix_new(2, 3, 4);
+    stencil_matrix_free(matrix);
+
+    omp_set_num_threads(2);
+
+    #pragma omp parallel num_threads(2)
+    {
+        fprintf(stdout, "Hello OpenMP\n");
+    }
+
+    return EXIT_SUCCESS;
+}
