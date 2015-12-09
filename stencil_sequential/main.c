52e6fcf9d44d382d194ca69f4961aaea8ffaf64d
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-12-09 22:45:54 +0100

    add sequential stencil build files

---
+++ stencil_sequential/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stencil/matrix.h>
+
+int main(int argc, char **argv)
+{ 
+    fprintf(stdout, "Hello sequential\n");
+
+    stencil_matrix_t *matrix = stencil_matrix_new(2, 3, 4);
+    stencil_matrix_free(matrix);
+
+    return EXIT_SUCCESS;
+}
