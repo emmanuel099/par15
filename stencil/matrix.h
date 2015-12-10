9c440a28f08d45f1e7db737dcf4e92ed741db56c
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-12-10 23:29:56 +0100

    Add some documentation to stencil_matrix

--- stencil/matrix.h
+++ stencil/matrix.h
@@ -4,7 +4,19 @@
 struct stencil_matrix;
 typedef struct stencil_matrix stencil_matrix_t;
 
+/**
+ * Creates a new matrix of size \a rows rows by \a cols columns.
+ *
+ * @param rows Number of rows
+ * @param cols Number of columns
+ *
+ * @return A pointer to a matrix initialized with 0 values, NULL on failure.
+ */
 stencil_matrix_t *stencil_matrix_new(size_t rows, size_t cols);
+
+/**
+ * Frees the memory of a matrix \a matrix.
+ */
 void stencil_matrix_free(stencil_matrix_t *matrix);
 
 // TODO provide setter/getter to access and set the values of a matrix
