4e49d1b5697e4f2dfeda0b779ab1b0e727adca48
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2015-12-16 11:56:07 +0100

    add a mutable row and a copy row iterator to stencil matrix

--- stencil/matrix.c
+++ stencil/matrix.c
@@ -1,25 +1,39 @@
 #include <stdarg.h>
 #include <assert.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "matrix.h"
 
 struct stencil_matrix {
-    size_t rows;
-    size_t cols;
+    ssize_t rows;
+    ssize_t cols;
     double *values;
 };
 
-static inline size_t to_index(stencil_matrix_t *matrix, size_t row, size_t col)
+struct stencil_matrix_mutable_row_iterator {
+    ssize_t row;
+    stencil_matrix_t *matrix;
+    double *values;
+};
+
+struct stencil_matrix_copy_row_iterator {
+    ssize_t row;
+    stencil_matrix_t *matrix;
+    double *values;
+};
+
+static inline ssize_t to_index(stencil_matrix_t *matrix, ssize_t row, ssize_t col)
 {
     return row * matrix->cols + col;
 }
 
-stencil_matrix_t *stencil_matrix_new(size_t rows, size_t cols)
+stencil_matrix_t *stencil_matrix_new(ssize_t rows, ssize_t cols)
 {
-    const size_t len = rows * cols;
+    assert(rows >= 0);
+    assert(cols >= 0);
+
+    const ssize_t len = rows * cols;
 
     double *values = (double *)malloc(len * sizeof(double));
     if (!values) {
@@ -38,9 +52,8 @@ stencil_matrix_t *stencil_matrix_new(size_t rows, size_t cols)
     return matrix;
 
 exit_matrix:
-    free(matrix);
+    free(values);
 exit_values:
-    fprintf(stderr, "Could not create matrix");
     return NULL;
 }
 
@@ -54,20 +67,158 @@ void stencil_matrix_free(stencil_matrix_t *matrix)
     free(matrix);
 }
 
-double stencil_matrix_get(stencil_matrix_t *matrix, size_t row, size_t col)
+double stencil_matrix_get(stencil_matrix_t *matrix, ssize_t row, ssize_t col)
 {
     assert(matrix);
-    assert(row <= matrix->rows);
-    assert(col <= matrix->cols);
+    assert(0 <= row && row < matrix->rows);
+    assert(0 <= col && col < matrix->cols);
 
     return matrix->values[to_index(matrix, row, col)];
 }
 
-void stencil_matrix_set(stencil_matrix_t *matrix, size_t row, size_t col, double value)
+double *stencil_matrix_get_ptr(stencil_matrix_t *matrix, ssize_t row, ssize_t col)
 {
     assert(matrix);
-    assert(row <= matrix->rows);
-    assert(col <= matrix->cols);
+    assert(0 <= row && row < matrix->rows);
+    assert(0 <= col && col < matrix->cols);
+
+    return matrix->values + to_index(matrix, row, col);
+}
+
+void stencil_matrix_set(stencil_matrix_t *matrix, ssize_t row, ssize_t col, double value)
+{
+    assert(matrix);
+    assert(0 <= row && row < matrix->rows);
+    assert(0 <= col && col < matrix->cols);
 
     matrix->values[to_index(matrix, row, col)] = value;
+}
+
+stencil_matrix_mutable_row_iterator_t *stencil_matrix_mutable_row_iterator_new(stencil_matrix_t *matrix)
+{
+    assert(matrix);
+
+    stencil_matrix_mutable_row_iterator_t *it = (stencil_matrix_mutable_row_iterator_t *)malloc(sizeof(stencil_matrix_mutable_row_iterator_t));
+    if (!it) {
+        return NULL;
+    }
+    it->row = -1;
+    it->matrix = matrix;
+    it->values = NULL;
+
+    return it;
+}
+
+stencil_matrix_mutable_row_iterator_t *stencil_matrix_mutable_row_iterator_next(stencil_matrix_mutable_row_iterator_t *it)
+{
+    assert(it);
+
+    stencil_matrix_t *matrix = it->matrix;
+
+    it->row++;
+    if (it->row < matrix->rows) {
+        it->values = stencil_matrix_get_ptr(matrix, it->row, 0);
+        return it;
+    } else {
+        return NULL;
+    }
+}
+
+void stencil_matrix_mutable_row_iterator_skip(stencil_matrix_mutable_row_iterator_t *it, ssize_t rows)
+{
+    assert(it);
+    assert(rows >= 0);
+
+    it->row += rows;
+}
+
+void stencil_matrix_mutable_row_iterator_free(stencil_matrix_mutable_row_iterator_t *it)
+{
+    if (!it) {
+        return;
+    }
+
+    free(it);
+}
+
+double stencil_matrix_mutable_row_iterator_get(stencil_matrix_mutable_row_iterator_t *it, ssize_t col)
+{
+    assert(it);
+    assert(0 <= col && col < it->matrix->cols);
+
+    return it->values[col];
+}
+
+void stencil_matrix_mutable_row_iterator_set(stencil_matrix_mutable_row_iterator_t *it, ssize_t col, double value)
+{
+    assert(it);
+    assert(0 <= col && col < it->matrix->cols);
+
+    it->values[col] = value;
+}
+
+stencil_matrix_copy_row_iterator_t *stencil_matrix_copy_row_iterator_new(stencil_matrix_t *matrix)
+{
+    assert(matrix);
+
+    double *values = (double *)malloc(matrix->cols * sizeof(double));
+    if (!values) {
+        goto exit_values;
+    }
+
+    stencil_matrix_copy_row_iterator_t *it = (stencil_matrix_copy_row_iterator_t *)malloc(sizeof(stencil_matrix_copy_row_iterator_t));
+    if (!it) {
+        goto exit_it;
+    }
+    it->row = -1;
+    it->matrix = matrix;
+    it->values = values;
+
+    return it;
+
+exit_it:
+    free(values);
+exit_values:
+    return NULL;
+}
+
+stencil_matrix_copy_row_iterator_t *stencil_matrix_copy_row_iterator_next(stencil_matrix_copy_row_iterator_t *it)
+{
+    assert(it);
+
+    stencil_matrix_t *matrix = it->matrix;
+
+    it->row++;
+    if (it->row < matrix->rows) {
+        memcpy(it->values, stencil_matrix_get_ptr(matrix, it->row, 0), matrix->cols);
+        return it;
+    } else {
+        return NULL;
+    }
+}
+
+void stencil_matrix_copy_row_iterator_skip(stencil_matrix_copy_row_iterator_t *it, ssize_t rows)
+{
+    assert(it);
+    assert(rows >= 0);
+
+    it->row += rows;
+}
+
+void stencil_matrix_copy_row_iterator_free(stencil_matrix_copy_row_iterator_t *it)
+{
+    if (!it) {
+        return;
+    }
+
+    free(it->values);
+    free(it);
+}
+
+double stencil_matrix_copy_row_iterator_get(stencil_matrix_copy_row_iterator_t *it, ssize_t col)
+{
+    assert(it);
+    assert(0 <= col && col < it->matrix->cols);
+
+    return it->values[col];
 }
--- stencil/matrix.h
+++ stencil/matrix.h
@@ -6,6 +6,12 @@
 struct stencil_matrix;
 typedef struct stencil_matrix stencil_matrix_t;
 
+struct stencil_matrix_mutable_row_iterator;
+typedef struct stencil_matrix_mutable_row_iterator stencil_matrix_mutable_row_iterator_t;
+
+struct stencil_matrix_copy_row_iterator;
+typedef struct stencil_matrix_copy_row_iterator stencil_matrix_copy_row_iterator_t;
+
 /**
  * Creates a new matrix of size \a rows rows by \a cols columns.
  *
@@ -14,7 +20,7 @@ typedef struct stencil_matrix stencil_matrix_t;
  *
  * @return A pointer to a matrix initialized with 0 values, NULL on failure.
  */
-stencil_matrix_t *stencil_matrix_new(size_t rows, size_t cols);
+stencil_matrix_t *stencil_matrix_new(ssize_t rows, ssize_t cols);
 
 /**
  * Frees the memory of a matrix \a matrix.
@@ -30,7 +36,18 @@ void stencil_matrix_free(stencil_matrix_t *matrix);
  *
  * @return Value at position [\a row, \a col]
  */
-double stencil_matrix_get(stencil_matrix_t *matrix, size_t row, size_t col);
+double stencil_matrix_get(stencil_matrix_t *matrix, ssize_t row, ssize_t col);
+
+/**
+ * Returns a pointer to the element at position [\a row, \a col] of matrix \a matrix.
+ *
+ * @param matrix A pointer to the matrix (must be valid)
+ * @param row Row index (must be in range [0, matrix.rows])
+ * @param col Column index (must be in range [0, matrix.cols])
+ *
+ * @return Pointer to the element at position [\a row, \a col]
+ */
+double *stencil_matrix_get_ptr(stencil_matrix_t *matrix, ssize_t row, ssize_t col);
 
 /**
  * Sets the value at position [\a row, \a col] of matrix \a matrix.
@@ -40,6 +57,45 @@ double stencil_matrix_get(stencil_matrix_t *matrix, size_t row, size_t col);
  * @param col Column index (must be in range [0, matrix.cols])
  * @param value Value which should be set
  */
-void stencil_matrix_set(stencil_matrix_t *matrix, size_t row, size_t col, double value);
+void stencil_matrix_set(stencil_matrix_t *matrix, ssize_t row, ssize_t col, double value);
+
+
+#define stencil_matrix_row_iterator_next(it) _Generic((it), \
+        stencil_matrix_mutable_row_iterator_t *: stencil_matrix_mutable_row_iterator_next, \
+        stencil_matrix_copy_row_iterator_t *: stencil_matrix_copy_row_iterator_next \
+        )(it)
+
+#define stencil_matrix_row_iterator_skip(it, rows) _Generic((it), \
+        stencil_matrix_mutable_row_iterator_t *: stencil_matrix_mutable_row_iterator_skip, \
+        stencil_matrix_copy_row_iterator_t *: stencil_matrix_copy_row_iterator_skip \
+        )(it, rows)
+
+#define stencil_matrix_row_iterator_free(it) _Generic((it), \
+        stencil_matrix_mutable_row_iterator_t *: stencil_matrix_mutable_row_iterator_free, \
+        stencil_matrix_copy_row_iterator_t *: stencil_matrix_copy_row_iterator_free \
+        )(it)
+
+#define stencil_matrix_row_get(row, col) _Generic((row), \
+        stencil_matrix_mutable_row_iterator_t *: stencil_matrix_mutable_row_iterator_get, \
+        stencil_matrix_copy_row_iterator_t *: stencil_matrix_copy_row_iterator_get \
+        )(row, col)
+
+#define stencil_matrix_row_set(row, col, value) _Generic((row), \
+        stencil_matrix_mutable_row_iterator_t *: stencil_matrix_mutable_row_iterator_set \
+        )(row, col, value)
+
+
+stencil_matrix_mutable_row_iterator_t *stencil_matrix_mutable_row_iterator_new(stencil_matrix_t *matrix);
+stencil_matrix_mutable_row_iterator_t *stencil_matrix_mutable_row_iterator_next(stencil_matrix_mutable_row_iterator_t *it);
+void stencil_matrix_mutable_row_iterator_skip(stencil_matrix_mutable_row_iterator_t *it, ssize_t rows);
+void stencil_matrix_mutable_row_iterator_free(stencil_matrix_mutable_row_iterator_t *it);
+double stencil_matrix_mutable_row_iterator_get(stencil_matrix_mutable_row_iterator_t *it, ssize_t col);
+void stencil_matrix_mutable_row_iterator_set(stencil_matrix_mutable_row_iterator_t *it, ssize_t col, double value);
+
+stencil_matrix_copy_row_iterator_t *stencil_matrix_copy_row_iterator_new(stencil_matrix_t *matrix);
+stencil_matrix_copy_row_iterator_t *stencil_matrix_copy_row_iterator_next(stencil_matrix_copy_row_iterator_t *it);
+void stencil_matrix_copy_row_iterator_skip(stencil_matrix_copy_row_iterator_t *it, ssize_t rows);
+void stencil_matrix_copy_row_iterator_free(stencil_matrix_copy_row_iterator_t *it);
+double stencil_matrix_copy_row_iterator_get(stencil_matrix_copy_row_iterator_t *it, ssize_t col);
 
 #endif // __STENCIL_MATRIX_H
