1b8285188972c8777420bf67e11b36603d275cee
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2016-01-03 19:26:13 +0100

    openmp: initialise temp vectors before we use them

--- stencil_openmp/main.c
+++ stencil_openmp/main.c
@@ -6,11 +6,7 @@
 
 #include <stencil/matrix.h>
 #include <stencil/vector.h>
-
-static double time_difference_ms(struct timeval t1, struct timeval t2)
-{
-    return (t2.tv_sec * 1000.0 + t2.tv_usec / 1000.0) - (t1.tv_sec * 1000.0 + t1.tv_usec / 1000.0);
-}
+#include <stencil/util.h>
 
 inline double stencil_five_point_kernel(const stencil_matrix_t *const matrix, size_t row, size_t col)
 {
@@ -48,20 +44,33 @@ static void five_point_stencil_with_tmp_matrix(stencil_matrix_t *matrix)
 
 static void five_point_stencil_with_two_vectors(stencil_matrix_t *matrix)
 {
-    const size_t rows = matrix->rows - 1;
     const size_t cols = matrix->cols - 1;
 
     struct timeval t1;
     gettimeofday(&t1, NULL);
 
-    #pragma omp parallel firstprivate(rows, cols) shared(matrix)
+    #pragma omp parallel firstprivate(cols) shared(matrix)
     {
-        stencil_vector_t *above = stencil_matrix_get_row(matrix, 0); // FIXME first block idx
-        #pragma omp barrier
+        const int thread = omp_get_thread_num();
+        const size_t rows_per_thread = (matrix->rows - 2) / omp_get_num_threads();
+
+        const size_t start_row = thread * rows_per_thread + 1;
+        const size_t end_row = start_row + rows_per_thread;
+
+        stencil_vector_t *above = stencil_vector_new(matrix->cols);
         stencil_vector_t *current = stencil_vector_new(matrix->cols);
 
-        #pragma omp for schedule(static) nowait
-        for (size_t row = 1; row < rows; row++) {
+        // calculate the first row (above doesn't contain any useful data yet)
+        for (size_t col = 1; col < cols; col++) {
+            const double value = stencil_five_point_kernel(matrix, start_row, col);
+            stencil_vector_set(above, col, value);
+        }
+
+        // wait until all threads have filled the first row
+        #pragma omp barrier
+
+        // calculate the remaining rows
+        for (size_t row = start_row + 1; row < end_row; row++) {
             for (size_t col = 1; col < cols; col++) {
                 const double value = stencil_five_point_kernel(matrix, row, col);
                 stencil_vector_set(current, col, value);
@@ -72,6 +81,9 @@ static void five_point_stencil_with_two_vectors(stencil_matrix_t *matrix)
             above = current;
             current = tmp;
         }
+
+        // copy back the last row
+        stencil_matrix_set_row(matrix, end_row - 1, above);
 
         stencil_vector_free(above);
         stencil_vector_free(current);
@@ -85,28 +97,44 @@ static void five_point_stencil_with_two_vectors(stencil_matrix_t *matrix)
 
 static void five_point_stencil_with_one_vector(stencil_matrix_t *matrix)
 {
-    const size_t rows = matrix->rows - 1;
     const size_t cols = matrix->cols - 1;
 
     struct timeval t1;
     gettimeofday(&t1, NULL);
 
-    #pragma omp parallel firstprivate(rows, cols) shared(matrix)
+    #pragma omp parallel firstprivate(cols) shared(matrix)
     {
-        stencil_vector_t *tmp = stencil_matrix_get_row(matrix, 0); // FIXME first block idx
+        const int thread = omp_get_thread_num();
+        const size_t rows_per_thread = (matrix->rows - 2) / omp_get_num_threads();
+
+        const size_t start_row = thread * rows_per_thread + 1;
+        const size_t end_row = start_row + rows_per_thread;
+
+        stencil_vector_t *vec = stencil_vector_new(matrix->cols);
+
+        // calculate the first row (vec doesn't contain any useful data yet)
+        for (size_t col = 1; col < cols; col++) {
+            const double value = stencil_five_point_kernel(matrix, start_row, col);
+            stencil_vector_set(vec, col, value);
+        }
+
+        // wait until all threads have filled the first row
         #pragma omp barrier
 
-        #pragma omp for schedule(static) nowait
-        for (size_t row = 1; row < rows; row++) {
+        // calculate the remaining rows
+        for (size_t row = start_row + 1; row < end_row; row++) {
             for (size_t col = 1; col < cols; col++) {
                 const double value = stencil_five_point_kernel(matrix, row, col);
                 // copy back the previosly calculated value before we overwrite it
-                stencil_matrix_set(matrix, row - 1, col, stencil_vector_get(tmp, col));
-                stencil_vector_set(tmp, col, value);
+                stencil_matrix_set(matrix, row - 1, col, stencil_vector_get(vec, col));
+                stencil_vector_set(vec, col, value);
             }
         }
 
-        stencil_vector_free(tmp);
+        // copy back the last row
+        stencil_matrix_set_row(matrix, end_row - 1, vec);
+
+        stencil_vector_free(vec);
     }
 
     struct timeval t2;
