f89e4cf9b5ce0cc557283234e0c3217b8f776f40
Philipp Paris <paris.philipp@gmail.com>
2016-01-03 15:53:25 +0100

    added new_randomized_matrix function
    renamed matrix_from_file to new_matrix_from_file
    cleanups

--- stencil/util.c
+++ stencil/util.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "util.h"
 
-stencil_matrix_t* matrix_from_file(const char* filepath)
+stencil_matrix_t* new_matrix_from_file(const char* filepath)
 {
     FILE *stream = fopen(filepath, "r");
     if (stream == NULL) {
@@ -34,15 +35,18 @@ stencil_matrix_t* matrix_from_file(const char* filepath)
 
     /* create matrix */
     stencil_matrix_t* matrix = stencil_matrix_new(rows, cols);
+    if (matrix == NULL) {
+        goto exit;
+    }
 
     /* read matrix values */
-    for (int row = 0; row < matrix->rows; ++row) {
+    for (size_t row = 0; row < matrix->rows; ++row) {
         if (getline(&line, &len, stream) == 0) {
             goto exit_matrix;
         }
 
         char* record = strtok(line, ";");
-        for (int col = 0; col < matrix->cols; ++col) {
+        for (size_t col = 0; col < matrix->cols; ++col) {
             if (record == NULL) {
                 goto exit_matrix;
             }
@@ -68,8 +72,8 @@ bool matrix_to_file(const stencil_matrix_t* matrix, const char* filepath)
         return false;
     }
 
-    for(int row = 0; row < matrix->rows; ++row) {
-        for(int col = 0; col < matrix->cols; ++col) {
+    for(size_t row = 0; row < matrix->rows; ++row) {
+        for(size_t col = 0; col < matrix->cols; ++col) {
            if (fprintf(stream, "%.3f;", stencil_matrix_get(matrix, row, col)) < 0) {
                fclose(stream);
                return false;
@@ -83,4 +87,22 @@ bool matrix_to_file(const stencil_matrix_t* matrix, const char* filepath)
 
     fclose(stream);
     return true;
+}
+
+stencil_matrix_t* new_randomized_matrix(int rows, int cols, int min_value, int max_value)
+{
+    stencil_matrix_t* matrix = stencil_matrix_new(rows, cols);
+    if (matrix == NULL) {
+        return NULL;
+    }
+
+    srand((unsigned int)time(NULL));
+
+    for (size_t row = 0; row < matrix->rows; ++row) {
+        for (size_t col = 0; col < matrix->cols; ++col) {
+            stencil_matrix_set(matrix, row, col, min_value + rand() % max_value);
+        }
+    }
+
+    return matrix;
 }
--- stencil/util.h
+++ stencil/util.h
@@ -7,15 +7,28 @@
 #include <stencil/matrix.h>
 
 /**
- * creates a matrix from the provided csv file \a filepath
+ * creates a new matrix with values from the provided csv file \a filepath
  * the first line of the file has to contain
  * the number of rows and number of columns
  *
  * @param filepath path to the csv file
  *
- * @return returns the matrix or NULL if an error has ocurred
+ * @return returns the matrix if successful or NULL if an error has ocurred
  */
-stencil_matrix_t* matrix_from_file(const char* filepath);
+stencil_matrix_t* new_matrix_from_file(const char* filepath);
+
+/**
+ * creates a new matrix with randomized values
+ * the values are in the interval [\a min_value, \a max_value)
+ *
+ * @param rows number of rows of the matrix
+ * @param cols number of columns of the matrix
+ * @param min_value lower end of the value interval
+ * @param max_value upper end of the value interval
+ *
+ * @returns returns the matrix if successful or NULL if an error has ocurred
+ */
+stencil_matrix_t* new_randomized_matrix(int rows, int cols, int min_value, int max_value);
 
 /**
  * writes the matrix \a matrix to the csv file \a filepath
