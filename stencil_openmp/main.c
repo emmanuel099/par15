996f4f982ff99fc4c8c980155cfd0b9887dd920a
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2016-01-04 15:14:47 +0100

    remove five point stencil with two temp vectors from stencil_openmp

--- stencil_openmp/main.c
+++ stencil_openmp/main.c
@@ -42,59 +42,6 @@ static void five_point_stencil_with_tmp_matrix(stencil_matrix_t *matrix)
     stencil_matrix_free(tmp_matrix);
 }
 
-static void five_point_stencil_with_two_vectors(stencil_matrix_t *matrix)
-{
-    const size_t cols = matrix->cols - 1;
-
-    struct timeval t1;
-    gettimeofday(&t1, NULL);
-
-    #pragma omp parallel shared(matrix)
-    {
-        const int thread = omp_get_thread_num();
-        const size_t rows_per_thread = (matrix->rows - 2) / omp_get_num_threads();
-
-        const size_t start_row = thread * rows_per_thread + 1;
-        const size_t end_row = start_row + rows_per_thread;
-
-        stencil_vector_t *above = stencil_vector_new(matrix->cols);
-        stencil_vector_t *current = stencil_vector_new(matrix->cols);
-
-        // calculate the first row (above doesn't contain any useful data yet)
-        for (size_t col = 1; col < cols; col++) {
-            const double value = stencil_five_point_kernel(matrix, start_row, col);
-            stencil_vector_set(above, col, value);
-        }
-
-        // wait until all threads have filled the first row
-        #pragma omp barrier
-
-        // calculate the remaining rows
-        for (size_t row = start_row + 1; row < end_row; row++) {
-            for (size_t col = 1; col < cols; col++) {
-                const double value = stencil_five_point_kernel(matrix, row, col);
-                stencil_vector_set(current, col, value);
-            }
-
-            stencil_matrix_set_row(matrix, row - 1, above);
-            stencil_vector_t *tmp = above;
-            above = current;
-            current = tmp;
-        }
-
-        // copy back the last row
-        stencil_matrix_set_row(matrix, end_row - 1, above);
-
-        stencil_vector_free(above);
-        stencil_vector_free(current);
-    }
-
-    struct timeval t2;
-    gettimeofday(&t2, NULL);
-
-    printf("five_point_stencil_with_two_vectors - elapsed time %fms\n", time_difference_ms(t1, t2));
-}
-
 static void five_point_stencil_with_one_vector(stencil_matrix_t *matrix)
 {
     const size_t cols = matrix->cols - 1;
@@ -155,7 +102,6 @@ int main(int argc, char **argv)
     stencil_matrix_t *matrix = stencil_matrix_new(10000, 20000);
     for (int i = 0; i < 6; i++) {
         five_point_stencil_with_tmp_matrix(matrix);
-        five_point_stencil_with_two_vectors(matrix);
         five_point_stencil_with_one_vector(matrix);
     }
     stencil_matrix_free(matrix);
