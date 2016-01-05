dce71cbbb6ac77afacbd8e0d55e9ea0ceef138bc
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2016-01-05 12:21:53 +0100

    Revert "fixed bugs in cilk and sequential implementations"
    Matrix setters take the boundary into account.
    
    This reverts commit af1f2e9b4f40930a32dfa2a6e0e1ad11656b44cf.

--- stencil_cilk/stencil_cilk.c
+++ stencil_cilk/stencil_cilk.c
@@ -70,9 +70,7 @@ static void five_point_stencil_with_two_vectors(stencil_matrix_t *matrix)
             stencil_vector_set(current, col, value);
         }
 
-
-        // copy back row values (without boundary! therefore do not use stencil_matrix_set_row)
-        memcpy(stencil_matrix_get_ptr(matrix, row - 1, 1), above->values + 1, (matrix->cols - 2) * sizeof(double));
+        stencil_matrix_set_row(matrix, row - 1, above);
         stencil_vector_t *tmp = above;
         above = current;
         current = tmp;
