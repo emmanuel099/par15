0ccf33ef1d9c6e8fe87778a1b28bb33145739fde
Philipp Paris <paris.philipp@gmail.com>
2016-01-05 17:09:06 +0100

    added test framework

---
+++ CMake/run_test.cmake
@@ -0,0 +1,35 @@
+# some argument checking:
+# test_cmd is the command to run with all its arguments
+
+set(OUT ${test_dir}/test.out)
+set(EXP ${input}.exp)
+
+if( NOT test_cmd )
+   message( FATAL_ERROR "Variable test_cmd not defined" )
+endif (NOT test_cmd)
+
+# EXP contains the name of the "expected" output file
+if( NOT EXP )
+   message( FATAL_ERROR "Variable EXP not defined" )
+endif( NOT EXP )
+
+# OUT contains the name of the output file the test_cmd will produce
+if( NOT OUT )
+   message( FATAL_ERROR "Variable OUT not defined" )
+endif( NOT OUT )
+
+execute_process(
+   COMMAND ${test_cmd} ${input}
+   OUTPUT_FILE ${OUT}
+   )
+
+execute_process(
+   COMMAND ${CMAKE_COMMAND} -E compare_files ${EXP} ${OUT}
+   RESULT_VARIABLE test_not_successful
+   OUTPUT_QUIET
+   ERROR_QUIET
+   )
+
+if( test_not_successful )
+   message( SEND_ERROR "${OUT} does not match ${EXP}!" )
+endif( test_not_successful )
---
+++ stencil_cilk/unit_test_tmp_matrix.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
+
+#include "stencil/util.h"
+#include "stencil_cilk.h"
+
+int main(int argc, char **argv)
+{
+    if (argv[1] == NULL) {
+        fprintf(stdout, "ERROR: file argument missing");
+        return EXIT_FAILURE;
+    }
+
+    stencil_matrix_t *matrix = new_matrix_from_file(argv[1]);
+    if (matrix == NULL) {
+        return EXIT_FAILURE;
+    }
+    cilk_stencil_tmp_matrix(matrix);
+    matrix_to_file(matrix, stdout);
+
+    free(matrix);
+    return EXIT_SUCCESS;
+}
