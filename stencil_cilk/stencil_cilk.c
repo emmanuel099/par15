72a41fbba2c395a38b5c810fd45c52e313a2dc2b
Philipp Paris <paris.philipp@gmail.com>
2016-01-05 20:23:44 +0100

    modified cilk_stencil_buffer_first_row to process matrices with
    rows modulo wokers != 0

--- stencil_cilk/stencil_cilk.c
+++ stencil_cilk/stencil_cilk.c
@@ -85,7 +85,7 @@ static void five_point_stencil_with_two_vectors(stencil_matrix_t *matrix)
 
 static stencil_vector_t* calc_first_row(stencil_matrix_t *matrix, size_t row)
 {
-    stencil_vector_t *vector = stencil_matrix_get_row(matrix, row);
+    stencil_vector_t *vector = stencil_vector_new(matrix->cols);
     const size_t cols = matrix->cols - matrix->boundary;
 
     for (size_t col = matrix->boundary; col < cols; col++) {
@@ -98,16 +98,22 @@ static stencil_vector_t* calc_first_row(stencil_matrix_t *matrix, size_t row)
 
 static void update_values(stencil_matrix_t *matrix, size_t start_row, size_t end_row)
 {
-    const size_t rows = end_row - matrix->boundary;
+    const size_t rows = end_row - 1;
     const size_t cols = matrix->cols - matrix->boundary;
 
     if (start_row >= end_row) {
         return;
     }
 
-    stencil_vector_t *tmp = stencil_matrix_get_row(matrix, 0);
+    stencil_vector_t *tmp = stencil_vector_new(matrix->cols);
 
-    for (size_t row = start_row; row < rows; row++) {
+    // calculate the first row
+    for (size_t col = matrix->boundary; col < cols; col++) {
+        stencil_vector_set(tmp, col, stencil_five_point_kernel(matrix, start_row, col));
+    }
+
+    // calculate the remaining rows
+    for (size_t row = start_row + 1; row < rows; row++) {
         for (size_t col = matrix->boundary; col < cols; col++) {
             const double value = stencil_five_point_kernel(matrix, row, col);
             // copy back the previosly calculated value before we overwrite it
@@ -115,7 +121,6 @@ static void update_values(stencil_matrix_t *matrix, size_t start_row, size_t end
             stencil_vector_set(tmp, col, value);
         }
     }
-
     // copy back the last row
     stencil_matrix_set_row(matrix, rows - 1, tmp);
 
@@ -127,28 +132,31 @@ double cilk_stencil_buffer_first_row(stencil_matrix_t *matrix)
     const size_t boundary = matrix->boundary * 2;
     const size_t workers = __cilkrts_get_nworkers();
     const size_t rows_per_worker = (matrix->rows - boundary) / workers; // rows per worker without 2 overlapping rows
-
     stencil_vector_t **first_row_vectors = malloc(workers * sizeof(stencil_vector_t*));
 
     struct timeval t1;
     gettimeofday(&t1, NULL);
 
     for (size_t i = 0; i < workers; i++) {
-        first_row_vectors[i] = cilk_spawn calc_first_row(matrix, i * rows_per_worker + 1);
+        first_row_vectors[i] = cilk_spawn calc_first_row(matrix, i * rows_per_worker + matrix->boundary);
     }
     cilk_sync;
 
     /* calculate other values */
-    for (size_t i = 0; i < workers; i++) {
-        size_t start_row = i * rows_per_worker + boundary;
-        size_t end_row = start_row + rows_per_worker;
+    for (size_t i = 0; i < (workers - 1); i++) {
+        const size_t start_row = i * rows_per_worker + matrix->boundary + 1;
+        const size_t end_row = start_row + rows_per_worker;
         cilk_spawn update_values(matrix, start_row, end_row);
     }
+    /* last worker calculates more than rows_per_worker if row % workers != 0*/
+    const size_t start_row = (workers - 1) * rows_per_worker + matrix->boundary + 1;
+    const size_t end_row = start_row + rows_per_worker + (matrix->rows - boundary) % workers;
+    cilk_spawn update_values(matrix, start_row, end_row);
     cilk_sync;
 
     /* copy first row vectors values back to matrix*/
     for (size_t i = 0; i < workers; i++) {
-        stencil_matrix_set_row(matrix, i * rows_per_worker + 1, first_row_vectors[i]);
+        stencil_matrix_set_row(matrix, i * rows_per_worker + matrix->boundary, first_row_vectors[i]);
     }
 
     struct timeval t2;
