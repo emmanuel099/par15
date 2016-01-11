534eed3bfa1a98ed4e30538a9a5d8159774d720c
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2016-01-11 19:37:13 +0100

    make it possible to run multiple iterations of five_point_stencil

--- stencil_mpi/main.c
+++ stencil_mpi/main.c
@@ -16,23 +16,27 @@ int main(int argc, char **argv)
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    stencil_matrix_t *matrix = NULL;
     if (rank == MASTER) {
         fprintf(stdout, "Hello mpi\n");
 
-        stencil_matrix_t *matrix = stencil_matrix_new(10000, 20000, 1);
+        matrix = stencil_matrix_new(10000, 20000, 1);
         if (matrix == NULL) {
             return EXIT_FAILURE;
         }
+    }
 
-        printf("\nfive_point_stencil_with_one_vector: \n");
-        for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++) {
+        if (rank == MASTER) {
             double time = five_point_stencil_host(matrix, 6);
             printf("elapsed time %fms\n", time);
+        } else {
+            five_point_stencil_client();
         }
+    }
 
+    if (rank == MASTER) {
         stencil_matrix_free(matrix);
-    } else {
-        five_point_stencil_client();
     }
 
     MPI_Finalize();
