0374735b6c94c7c4495c52230e149aec2c1faf8c
Emmanuel Pescosta <emmanuelpescosta099@gmail.com>
2016-01-22 20:47:30 +0100

    fix script

--- benchmarks/create_diagrams.sh
+++ benchmarks/create_diagrams.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-mkdir -p .gptmp
-
 for benchmark in *benchmark.*.csv; do
-    rm -Rf ".gptmp/*"
+    mkdir -p .gptmp
     awk '/rows/{filename=".gptmp/tmp"NR".csv"}; {print >filename}' "${benchmark}"
     for b in .gptmp/tmp*.csv; do
         echo "${b}"
         gnuplot -e "infile='${b}'; outname='${benchmark%.*}'" "bench.gnuplot"
     done
+    rm -Rf ".gptmp"
 done
-
-rm -Rf ".gptmp"
 
 exit 0
